8b881429f461e1b9ff525e5064a0496b4ee6d54b
youngerfill <bergoid@users.noreply.github.com>
2025-10-27 15:42:00 +0100

    067

--- exercises/067_comptime2.zig
+++ exercises/067_comptime2.zig
@@ -35,18 +35,22 @@ pub fn main() void {
     // In this contrived example, we've decided to allocate some
     // arrays using a variable count! But something's missing...
     //
-    var count = 0;
+    comptime var count = 0;
 
     count += 1;
+    //@compileLog("Count at compile time: ", count);
     const a1: [count]u8 = .{'A'} ** count;
 
     count += 1;
+    //@compileLog("Count at compile time: ", count);
     const a2: [count]u8 = .{'B'} ** count;
 
     count += 1;
+    //@compileLog("Count at compile time: ", count);
     const a3: [count]u8 = .{'C'} ** count;
 
     count += 1;
+    //@compileLog("Count at compile time: ", count);
     const a4: [count]u8 = .{'D'} ** count;
 
     print("{s} {s} {s} {s}\n", .{ a1, a2, a3, a4 });
