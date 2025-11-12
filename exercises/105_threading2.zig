49c2e266227e929fdd81451e405d6827e5564377
youngerfill <33162647+youngerfill@users.noreply.github.com>
2025-11-12 16:47:09 +0100

    105

--- exercises/105_threading2.zig
+++ exercises/105_threading2.zig
@@ -81,7 +81,8 @@ pub fn main() !void {
         defer handle1.join();
 
         // Second thread to calculate the minus numbers.
-        ???
+        const handle2 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_minus, 3, count });
+        defer handle2.join();
         
     }
     // Here we add up the results.
